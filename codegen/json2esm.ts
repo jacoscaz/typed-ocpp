8572bf636d9373e9e745f7a4b8c823b155a678ce
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-03-19 19:12:53 +0100

    cleans up, adds a few comments

--- codegen/json2esm.ts
+++ codegen/json2esm.ts
@@ -10,13 +10,17 @@ import { readCLIParams, readSchemaFiles, output_file_header } from './common.js'
 (async () => {
 
   const { input_dir_abspath, output_file_abspath, mode } = readCLIParams();
-
   let output_file_data = output_file_header;
-
   const { schemas, definitions } = await readSchemaFiles(mode, input_dir_abspath);
 
-  output_file_data += `\n\nexport const definitions: any = ${JSON.stringify(definitions, null, 2)};`;
+  // First declare and initialize the shared dictionary of definitions, so that
+  // we may reference it when printing the schemas.
+  output_file_data += `\n\nconst definitions: any = ${JSON.stringify(definitions, null, 2)};`;
 
+  // Now we declare and initialize each schema separately, taking care of
+  // skipping the `definitions` property during JSON serialization and making
+  // room for an injected reference to the instance of the shared definitions
+  // dictionary created above.
   for (let [schema_name, schema_defn] of Object.entries(schemas)) {
     schema_defn = { ...schema_defn, definitions: undefined };
     const serialized_defn = `{\n  definitions,  ${JSON.stringify(schema_defn, null, 2).slice(1)}`;
--- codegen/json2types.ts
+++ codegen/json2types.ts
@@ -14,32 +14,32 @@ import { readCLIParams, readSchemaFiles, output_file_header } from './common.js'
 (async () => {
 
   const { input_dir_abspath, output_file_abspath, mode } = readCLIParams();
-
   let output_file_data = output_file_header;
-
   const { schemas, definitions } = await readSchemaFiles(mode, input_dir_abspath);
 
+  // First we convert each shared definition of a child JSON schema to a type
+  // and append it to the resulting file, so that it may be referenced by the
+  // types for root-level JSON schemas created below.
   for (let [ schema_name, schema_defn ] of Object.entries(definitions)) {
-
+    // A simpler `schema_defn.definitions = definitions` assignment results
+    // in what looks like infinite looping when calling `compile()`, likely
+    // due to the recursivity of the resulting hierarchy of objects.
     schema_defn = { ...schema_defn, definitions }; 
-
     const compiled_type = await compile(schema_defn, schema_name, {
       ignoreMinAndMaxItems: true,
       declareExternallyReferenced: false,
     });
-
     output_file_data += compiled_type;
   }
 
+  // We can now generate types for root-level JSON schemas, which will
+  // reference the ones created above for shared definitions.
   for (const [schema_name, schema_defn] of Object.entries(schemas)) {
-        
     const compiled_type = await compile(schema_defn, schema_name, {
       ignoreMinAndMaxItems: true,
       declareExternallyReferenced: false,
     });
-
     output_file_data += compiled_type;
-      
   }
 
   // Remove comments
