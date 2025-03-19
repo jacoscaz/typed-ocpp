9902f27a029c15fc0c473d3c6cc68c2631209e01
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-03-19 17:17:49 +0100

    adds test focusing on the use of Celsius units in OCPP 1.6 StopTransaction calls as per https://github.com/jacoscaz/typed-ocpp/issues/7

--- src/ocpp16/call.test.ts
+++ src/ocpp16/call.test.ts
@@ -21,23 +21,62 @@ describe('OCPP16 - Call', () => {
 
   describe('validation', () => {
   
-    it('minimal notification', () => {
-      deepStrictEqual(validate([MessageType.CALL, 'test', Action.BootNotification, {
-        chargePointModel: 'test',
-        chargePointVendor: '55',
-      }]), true);
-      deepStrictEqual(validate.errors, []);
+    describe('BootNotification', () => {
+
+      it('minimal notification', () => {
+        deepStrictEqual(validate([MessageType.CALL, 'test', Action.BootNotification, {
+          chargePointModel: 'test',
+          chargePointVendor: '55',
+        }]), true);
+        deepStrictEqual(validate.errors, []);
+      });
+  
+      it('invalid notification (missing model)', () => {
+        deepStrictEqual(validate([MessageType.CALL, 'test', Action.BootNotification, {
+          chargePointVendor: '55',
+        }]), false);
+        deepStrictEqual(validate.errors, [
+          "Invalid OCPP call:  must have required property 'chargePointModel'"
+        ]);
+      });
+
     });
 
-    it('invalid notification (missing model)', () => {
-      deepStrictEqual(validate([MessageType.CALL, 'test', Action.BootNotification, {
-        chargePointVendor: '55',
-      }]), false);
-      deepStrictEqual(validate.errors, [
-        "Invalid OCPP call:  must have required property 'chargePointModel'"
-      ]);
+    describe('StopTransaction', () => {
+
+      it('Using "Celsius" unit in sampledValue array', () => {
+        const call = [ 
+          2, 
+          "983728ed-5d83-467f-be81-81282b93e8b2", 
+          "StopTransaction", 
+          { 
+            idTag: "1347738830", 
+            meterStop: 9972365, 
+            reason: "EVDisconnected", 
+            timestamp: "2025-03-17T23:33:29.456Z", 
+            transactionData: [ 
+              { 
+                sampledValue: [ 
+                  { 
+                    context: "Transaction.End",
+                    format: "Raw", 
+                    location: "Body", 
+                    measurand: "Temperature", 
+                    unit: "Celsius", 
+                    value: "60",
+                  }
+                ],
+                timestamp: "2025-03-17T23:33:29.456Z"
+              }
+            ],
+            transactionId: 1705987302
+          }
+        ];
+        deepStrictEqual(validate(call), true);
+      });
+      
     });
 
-});
+  });
 
 });
