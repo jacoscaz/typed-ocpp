d9c3fc602da9c5e37366eba76542606c04d5b993
Jacopo Scazzosi <jacopo@scazzosi.com>
2025-04-02 10:19:27 +0200

    adds a few more tests to ensure that exported types and values behave as expected

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,7 +6,46 @@ import { OCPP21 } from './index.js';
 import assert from 'node:assert';
 import { describe, it } from 'node:test';
 
-describe('schemas', () => {
+describe('Exports', () => {
+
+  it('The OCPP16 namespace exports the Action, MessageType and ErrorCode enums as both values and types', () => {
+    const action: OCPP16.Action = OCPP16.Action.Authorize;
+    const error_code: OCPP16.ErrorCode = OCPP16.ErrorCode.GenericError;
+    const message_type: OCPP16.MessageType = OCPP16.MessageType.CALL;
+  });
+
+  it('The OCPP20 namespace exports the Action, MessageType and ErrorCode enums as both values and types', () => {
+    const action: OCPP20.Action = OCPP20.Action.Authorize;
+    const error_code: OCPP20.ErrorCode = OCPP20.ErrorCode.GenericError;
+    const message_type: OCPP20.MessageType = OCPP20.MessageType.CALL;
+  });
+
+  it('The OCPP21 namespace exports the Action, MessageType and ErrorCode enums as both values and types', () => {
+    const action: OCPP21.Action = OCPP21.Action.Authorize;
+    const error_code: OCPP21.ErrorCode = OCPP21.ErrorCode.GenericError;
+    const message_type: OCPP21.MessageType = OCPP21.MessageType.SEND;
+  });
+
+  it('The OCPP16 namespace exports the validation functions', () => {
+    assert(typeof OCPP16.validate === 'function', 'OCPP16.validate is not a function');
+    assert(typeof OCPP16.validateCall === 'function', 'OCPP16.validate is not a function');
+    assert(typeof OCPP16.validateCallError === 'function', 'OCPP16.validateCallError is not a function');
+    assert(typeof OCPP16.validateCallResult === 'function', 'OCPP16.validateCallResult is not a function');
+  });
+
+  it('The OCPP20 namespace exports the validation functions', () => {
+    assert(typeof OCPP20.validate === 'function', 'OCPP16.validate is not a function');
+    assert(typeof OCPP20.validateCall === 'function', 'OCPP16.validate is not a function');
+    assert(typeof OCPP20.validateCallError === 'function', 'OCPP16.validateCallError is not a function');
+    assert(typeof OCPP20.validateCallResult === 'function', 'OCPP16.validateCallResult is not a function');
+  });
+
+  it('The OCPP21 namespace exports the validation functions', () => {
+    assert(typeof OCPP21.validate === 'function', 'OCPP16.validate is not a function');
+    assert(typeof OCPP21.validateCall === 'function', 'OCPP16.validate is not a function');
+    assert(typeof OCPP21.validateCallError === 'function', 'OCPP16.validateCallError is not a function');
+    assert(typeof OCPP21.validateCallResult === 'function', 'OCPP16.validateCallResult is not a function');
+  });
 
   it('The OCPP16 namespace exports schemas', () => {
     assert(typeof OCPP16.schemas === 'object', 'OCPP16.schemas is not an object');    
